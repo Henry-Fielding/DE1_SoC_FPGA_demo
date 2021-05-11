e27f6f411495365afb5554ab666fdee38d7e6736
Henry-Fielding <78751620+Henry-Fielding@users.noreply.github.com>
2021-05-11 09:28:15 +0100

    Initial commit
    - Created new project
    - Added top level module file

---
+++ MiniProjectTopLevel.v
@@ -0,0 +1,19 @@
+/*
+ * Mini-project Top level
+ * ------------------------
+ * By: Henry Fielding
+ * For: University of Leeds
+ * Date: 11th May 2021
+ *
+ * Short Description
+ * -----------------
+ * This module is designed to designed to demonstrate the FPGA skills
+ * developed throughout this module.
+ *
+ */
+ 
+module MiniProjectTopLevel (
+    
+);
+
+endmodule 
