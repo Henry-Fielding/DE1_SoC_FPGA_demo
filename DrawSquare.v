00fa2e8f9ef281884041610d0829102c8ae05361
Henry-Fielding <78751620+Henry-Fielding@users.noreply.github.com>
2021-05-11 09:59:57 +0100

    DrawSquare - Added state machine
    - Added state machine
    - added pseudocode for functionaility

--- DrawSquare.v
+++ DrawSquare.v
@@ -16,20 +16,48 @@ module DrawSquare #(
 	parameter CLOCK_FREQ = 50000000
 )(
 	// declare ports
+	input 			clock,
+	input				reset,
 	input	[ 7:0]	xOrigin,
 	input	[ 8:0]	yOrigin,
 	input [ 7:0]	width,
 	input [ 8:0]	height,
 	input [15:0]	pixelData,
+	
 	output 			ready
 );
 
 //
 // Declare statemachine registers and parameters
 //
-reg state
+reg	[1:0]	state;
+localparam	IDLE_STATE			=	2'd0;
+localparam	SET_STATE			=	2'd1;
+localparam	WAIT_STATE			=	2'd2;
+localparam	INCREMENT_STATE	=	2'd3;
 
-
+always @(posedge clock or posedge reset) begin
+	if (reset) begin
+	
+	
+	end else begin
+		case (state)
+			IDLE_STATE : begin // wait for next command
+				
+			end
+			
+			SET_STATE : begin // set current pixel
+				
+			end
+			
+			WAIT_STATE : begin // wait for lcd to finish write
+			
+			end
+			
+			INCREMENT_STATE : begin // increment pixel
+			
+			end
+end
 
 
 endmodule
