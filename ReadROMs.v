7fb79e60cbe6a0bb00cebbd6c0ffdf3c3a0fb1d0
Henry-Fielding <78751620+Henry-Fielding@users.noreply.github.com>
2021-05-13 11:44:35 +0100

    ReadROMs - added module logic
    - added multiplexer logic to output of ROMs

--- ReadROMs.v
+++ ReadROMs.v
@@ -11,25 +11,52 @@
  *
  */
 
-module ReadROM (
+module ReadROMs (
 	// declare ports
-	input clock,	// timing ports
-	input reset,
-	input ROM,		// general ports
-	input ROMAddr,
+	input 			clock,	// timing ports
+	input 			reset,
+	input [ 3:0]	ROM,		// general ports
+	input	[15:0]	ROMAddr,
 	
-	output reg	ROMOut,
-	output reg	Ready
+	output reg	[15:0]	ReadROMOut,
 );
 
 //
 // Declare local registers/wires
 //
+reg	[15:0]	ROMOutBus	[3:0];
 
 //
 // Instatiate modules
 //
+MarioWalk1	ROM0 (
+	// define port connections
+	.clock 	(clock			),
+	.address	(ROMAddr			),
+	.q			(ROMOutBus[0]	)
+);
+
+MarioWalk2	ROM1 (
+	// define port connections
+	.clock 	(clock			),
+	.address	(ROMAddr			),
+	.q			(ROMOutBus[1]	)
+);
+
+MarioWalk3	ROM2 (
+	// define port connections
+	.clock 	(clock			),
+	.address	(ROMAddr			),
+	.q			(ROMOutBus[2]	)
+);
 
 //
-// Declare statemachine registers and parameters
+// Declare module logic
 //
+
+always @(posedge clock) begin
+	ReadROMOut <= ROMOutBus[ROM]
+end
+
+endmodule
+		
