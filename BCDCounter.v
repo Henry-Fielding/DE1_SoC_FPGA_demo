95ed3bb2cd240eba037c0cd2bac725fb012c239d
Henry-Fielding <78751620+Henry-Fielding@users.noreply.github.com>
2021-05-15 17:48:46 +0100

    BCDCounter - added BCD conversion logic
    - added logic to check each nibble for BCD validity

--- BCDCounter.v
+++ BCDCounter.v
@@ -1,106 +1,94 @@
-/* BCD counter
- * ------------------------
- * By: Henry Fielding
- * For: University of Leeds
- * Date: 15th May 2021
- *
- * Short Description
- * -----------------
- * This module is designed to count up in binary coded decimal
- *
- */
+// BCD counter
+// ------------------------
+// By: Henry Fielding
+// For: University of Leeds
+// Date: 15th May 2021
+//
+// Short Description
+// -----------------
+// This module is designed to count up in binary coded decimal
 
 module BCDCounter #(
-	parameter SCORE_BITWIDTH = 24;
-	parameter SCORE_NIBBLES = SCORE_BITWIDTH / 4;
-	parameter NIBBLE_COUNTER_BITWIDTH = $clog2(SCORE_NIBBLES + 1)
+	// declare parameters
+	parameter COUNTER_DIGITS				= 6,
+	parameter COUNTER_BITWIDTH				= COUNTER_DIGITS * 4,
+	parameter NIBBLE_COUNTER_BITWIDTH	= $clog2(COUNTER_DIGITS + 1)
 )(
 	// declare ports
-	input					clock,	// timing ports
-	input					reset,
-	input					enable,
+	input		clock,
+	input		reset,
+	input		enable,
 	
-	//output reg	[SCORE_BITWIDTH-1:0]	totalScoreOutput
-	output reg	[23:0]	countValueOut
+	output reg 									ready,
+	output reg	[COUNTER_BITWIDTH-1:0]	countValue
 );
 
+// declare local registers
+reg	[COUNTER_DIGITS-1:0] 				countValueTemp;
+reg	[NIBBLE_COUNTER_BITWIDTH-1:0]		nibbleCounter;
+reg	[3:0] 									nibble;
 
-//reg [NIBBLE_COUNTER_BITWIDTH-1:0] nibbleCounter;
-reg [23:0] countValueTemp;
+// declare local parameters
+localparam ZERO_COUNT				= {(COUNTER_BITWIDTH){1'd0}};
+localparam ONE_COUNT					= {{(COUNTER_BITWIDTH-1){1'd0}}, 1'd1};
+localparam ZERO_NIBBLE_COUNTER	= {(NIBBLE_COUNTER_BITWIDTH){1'd0}};
+localparam ONE_NIBBLE_COUNTER		= {{(NIBBLE_COUNTER_BITWIDTH-1){1'd0}}, 1'd1};
+
+// declare state machine registers and statenames
+reg	[3:0]	state;
+localparam IDLE_STATE				= 4'd0;
+localparam READY_STATE				= 4'd1;
+localparam EXAMINE_NIBBLES_STATE	= 4'd3;
+localparam UPDATE_OUTPUT_STATE	= 4'd4;
+
 //
-//localparam ZERO_SCORE = {(SCORE_BITWIDTH){1'd0}};
-//localparam ONE_SCORE = {{(SCORE_BITWIDTH-1){1'd0}}, 1'd0};
-//localparam SIX_SCORE = {{(SCORE_BITWIDTH-3){1'd0}}, 3'd6};
-//localparam ZERO_NIBBLE_COUNTER = {(NIBBLE_COUNTER_BITWIDTH){1'd0}};
-//localparam ONE_NIBBLE_COUNTER = {{(NIBBLE_COUNTER_BITWIDTH-1){1'd0}}, 1'd0};
-
-reg [3:0] state;
-localparam IDLE_STATE = 4'd0;
-localparam READY_STATE = 4'd1;
-localparam UPDATE_SCORE_STATE = 4'd2;
-localparam EXAMINE_NIBBLES_STATE = 4'd3;
-localparam UPDATE_OUTPUT_STATE = 4'd4;
-
-//always @(posedge clock) begin
-//	totalScoreOutput <= 24'h0000AA;
+// define statemachine behaviour
 //
-//end
-
 always @(posedge clock or posedge reset) begin
 	if (reset) begin
-		//totalScore <= ZERO_SCORE;
-		//totalScoreOutput <= ZERO_SCORE;
-		totalScore <= 24'd0;
-		totalScoreOutput<= 24'd0;
-		//nibbleCounter <= ZERO_NIBBLE_COUNTER;
-		state <= IDLE_STATE;
+		ready				<= 1'b0;
+		countValueTemp	<= ZERO_COUNT;
+		countValue		<= ZERO_COUNT;
+		state				<= IDLE_STATE;
+		
 	end else begin
 		case (state)
-			IDLE_STATE : begin
-				ready <= 1'd1;
-				//totalScore <= 24'h0000AA;
-//				state <= UPDATE_OUTPUT_STATE;
+			//	wait to enable input to be disabled
+			IDLE_STATE : begin		
 				if (!enable) begin
 					state <= READY_STATE;
 				end
 			end
 			
-			READY_STATE : begin
-				ready <= 1'd1;
+			// increment counter when enable bit set
+			READY_STATE : begin		
+				ready <= 1'b1;
+				
 				if (enable) begin
-					ready <= 1'd0;
-					totalScoreOutput <= totalScoreOutput + 1;
-					state <= IDLE_STATE;
+					ready				<= 1'b0;
+					countValueTemp	<= countValueTemp + ONE_COUNT;
+					nibbleCounter	<= ZERO_NIBBLE_COUNTER;
+					state				<= EXAMINE_NIBBLES_STATE;
 				end
 			end
-//			
-//			UPDATE_SCORE_STATE : begin
-//				if (score) begin
-//					totalScore <= totalScore + 1;//ONE_SCORE;
-//					nibbleCounter <= ZERO_NIBBLE_COUNTER;
-//					state <= EXAMINE_NIBBLES_STATE;
-//				end else begin
-//					state <= IDLE_STATE;
-//				end
-//			end
-//			
-//			EXAMINE_NIBBLES_STATE : begin
-//				if(totalScore[(nibbleCounter*4)+:4] > 9) begin
-//					totalScore <= totalScore + (6 << (nibbleCounter*4));//SIX_SCORE;
-//				end
-//				nibbleCounter <= nibbleCounter + ONE_NIBBLE_COUNTER;
-//		
-//				if (nibbleCounter > SCORE_NIBBLES) begin
-//					state <= UPDATE_OUTPUT_STATE;
-//				end else begin
-//					state <= EXAMINE_NIBBLES_STATE;
-//				end
-//			end
 			
-//			UPDATE_OUTPUT_STATE : begin
-//				totalScoreOutput <= ;
-//				state <= IDLE_STATE;
-//			end
+			// check each nibble for 'BCD overflow' (nibble > 9)
+			EXAMINE_NIBBLES_STATE : begin
+				nibbleCounter <= nibbleCounter + ONE_NIBBLE_COUNTER;					// increment nibble
+				if(countValueTemp[(nibbleCounter * 4)+:4] > 9) begin					// check if current nibble is > 9
+					countValueTemp <= countValueTemp + (6 << (nibbleCounter * 4));	// if so add 6 to the offending nibble
+				end
+				
+				if (nibbleCounter > COUNTER_DIGITS) begin
+					state <= UPDATE_OUTPUT_STATE;
+				end
+			end
+			
+			// update output BCD value
+			UPDATE_OUTPUT_STATE : begin
+				countValue	<= countValueTemp;
+				state			<= IDLE_STATE;
+			end
 		endcase
 	end
 end
