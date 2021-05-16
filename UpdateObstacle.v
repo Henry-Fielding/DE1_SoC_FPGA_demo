7186930519c2675a9e904d289d52b1298b15a56b
Henry-Fielding <78751620+Henry-Fielding@users.noreply.github.com>
2021-05-16 22:25:53 +0100

    UpdateObstacles - added random generation
    -added random generation for obstacles through psuedorandom array

--- UpdateObstacle.v
+++ UpdateObstacle.v
@@ -15,46 +15,71 @@ module UpdateObstacle (
 	input					update,	// timing ports
 	input					reset,
 	input	[3:0]			speed,
+	input [7:0]			randomSeed,
 	
 	output reg	[ 7:0]	xSprite,
 	output reg	[ 8:0]	ySprite,
-	output reg	[ 3:0] 	spriteId //
+	output reg	[ 3:0] 	obstacleId, //
+	output reg				passed
 );
 
+reg	[7:0] ROMAddr;
+wire			randomNumber;
+
+RandomNumbers	ROM (
+	// define port connections
+	.clock 	(update			),
+	.address	(ROMAddr			),
+	.q			(randomNumber	)
+);
 //
 // Declare statemachine registers and parameters
 //
 reg	[3:0]	state;
-localparam	WAIT_RANDOM_STATE		=	4'd0;
-localparam	RESET_POSITION_STATE		=	4'd1;
-localparam	UPDATE_POSITION_STATE		=	4'd2;
+localparam	RESET_POSITION_STATE		=	4'd0;
+localparam	UPDATE_POSITION_STATE		=	4'd1;
 
 always @(posedge update or posedge reset) begin
 	if (reset) begin
 		state <= RESET_POSITION_STATE;
+		passed <= 1'b0;
+		ROMAddr <= randomSeed;
 	end else begin
 		case (state)
-			WAIT_RANDOM_STATE : begin // wait a random amount of time before deploying obstacle
-				//unused
-			
-			end
-			
 			RESET_POSITION_STATE : begin // reset to left of screen 
-				xSprite <= 63; 
+				passed <= 1'b0;
 				ySprite <= 419;
-				
+				// if random = 1 deploy high
+				// else deploy low
+				if (randomNumber) begin
+					xSprite <= 110;
+					obstacleId <= 8;
+				end else begin
+					xSprite <= 63;
+					obstacleId <= 6;
+				end
+
 				state <= UPDATE_POSITION_STATE;
+				ROMAddr <= ROMAddr + 1;
 			end
 			
 			UPDATE_POSITION_STATE : begin // gradually move left
+				passed <= 1'b0;
 				ySprite <= ySprite - speed;
+				
+				if (obstacleId == 6) begin
+					obstacleId <= 7;
+				end else if (obstacleId == 7) begin
+					obstacleId <= 6;
+				end
 				
 				if (ySprite <= 36 + (2*speed)) begin
 					state <= RESET_POSITION_STATE;
+					passed <= 1'b1;
 				end
 			end
 		endcase
 	end
 end
 
-endmodule 
+endmodule
