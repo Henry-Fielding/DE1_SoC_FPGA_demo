e7be082160d8b5aa74e478e26d61a5b3198af7ec
Henry-Fielding <78751620+Henry-Fielding@users.noreply.github.com>
2021-05-16 13:54:46 +0100

    UpdateScore_tb - functionalised test regime

--- simulation/UpdateScore_tb.v
+++ simulation/UpdateScore_tb.v
@@ -83,58 +83,18 @@ initial begin
 	$display("Stage 1: Update testing");
 	$display("------------------------");
 	reset_testbench();
+	
 	for (i = 1; i <= ((10**SCORE_DIGITS) - 1); i = i + 1) begin // check counter through full range
 		binary = i;
 		score_add();
-		@(posedge ready) 
-		
-		// binary to bcd
-		BCD = 0;
-		for (j = 0; j < 32; j = j + 1) begin
-			BCD = {BCD[SCORE_BITWIDTH-2:0], binary[31-j]};
-			
-			// check each bit
-			for (k = 0; k < SCORE_DIGITS; k = k + 1) begin
-				if (j < 31 && BCD[(k * 4)+:4] > 4) begin
-					BCD[(k * 4)+:4] = BCD[(k * 4)+:4] + 3;
-				end
-			end
-		end
-		
-		//BCD to display
-		for (j = SCORE_DIGITS; j > 0; j = j - 1) begin
-			nibble = BCD[(4 * (j - 1)) +:4]; // top nibble first
-			case (nibble)
-				DISPLAY_0	:	expectedSegment = 8'b00111111;
-				DISPLAY_1	:	expectedSegment = 8'b00000110;
-				DISPLAY_2	:	expectedSegment = 8'b01011011;
-				DISPLAY_3	:	expectedSegment = 8'b01001111;
-				DISPLAY_4	:	expectedSegment = 8'b01100110;
-				DISPLAY_5	:	expectedSegment = 8'b01101101;
-				DISPLAY_6	:	expectedSegment = 8'b01111101;
-				DISPLAY_7	:	expectedSegment = 8'b00000111;
-				DISPLAY_8	:	expectedSegment = 8'b11111111;
-				DISPLAY_9	:	expectedSegment = 8'b01100111;
-			endcase
-			expectedDisplay = {expectedDisplay[DISPLAY_MSB-9:0], expectedSegment};// bottom end of BCD
-		end
-		
+		autoverify_display();
 	end
-		
-		
-		
-		
-		
-		
-		
-		//autoverify_display();
-
 	
-//	if (fail) begin
-//		$display("Failed - see above errors");
-//	end else begin 
-//		$display("All tests successful");
-//	end
+	if (fail) begin
+		$display("Failed - see above errors");
+	end else begin 
+		$display("All tests successful");
+	end
 	
 //	//
 //	// Reset testing
@@ -181,42 +141,68 @@ end
 endtask
 
 //// wait for BCD counter output and compare to expected behaviour
-//task autoverify_counter();
-//begin
-//	fork : f
-//		// check if counter value matches expected
-//		begin
-//			@(posedge ready);
-//			binary_to_BCD();
-//			BCD_to_display();
-//			if (display != expectedDisplay) begin
-//				fail = 1;
-//				$display("fail at: \t count = %0h \t expected count = %0d ", countValue,  expectedCount);
-//			end
-//			disable f;
-//		end
-//		
-//		// check counter finished operation in expected time
-//		begin
-//			repeat(TIMEOUT_CYCLES) @(posedge clock);
-//			fail = 1;
-//			$display("timeout at: expected count = %0d ", expectedCount);
-//			disable f;
-//		end
-//	join
-//end
-//endtask
-
-
-task binary_to_BCD();
-begin 
-
+task autoverify_display();
+begin
+	fail = 0;
+	fork : f
+		// check if counter value matches expected
+		begin
+			@(posedge ready);
+			binary_to_BCD();
+			BCD_to_display();
+			if (display != expectedDisplay) begin
+				fail = 1;
+				$display("fail at: \t count = %0d \t exhibited display = %h \t expected display = %h ", binary, display, expectedDisplay);
+			end
+			disable f;
+		end
 		
+		// check counter finished operation in expected time
+		begin
+			repeat(TIMEOUT_CYCLES) @(posedge clock);
+			fail = 1;
+			$display("timeout at: expected count = %0d ", binary);
+			disable f;
+		end
+	join
 end
 endtask
 
+task binary_to_BCD();
+begin
+	BCD = 0;
+	for (j = 0; j < 32; j = j + 1) begin
+		BCD = {BCD[SCORE_BITWIDTH-2:0], binary[31-j]};
+		// check each bit
+		for (k = 0; k < SCORE_DIGITS; k = k + 1) begin
+			if (j < 31 && BCD[(k * 4)+:4] > 4) begin
+				BCD[(k * 4)+:4] = BCD[(k * 4)+:4] + 3;
+			end
+		end
+	end
+end
+endtask
 
-
+task BCD_to_display();
+begin
+	for (j = SCORE_DIGITS; j > 0; j = j - 1) begin
+		nibble = BCD[(4 * (j - 1)) +:4]; // top nibble first
+		case (nibble)
+			DISPLAY_0	:	expectedSegment = 8'b00111111;
+			DISPLAY_1	:	expectedSegment = 8'b00000110;
+			DISPLAY_2	:	expectedSegment = 8'b01011011;
+			DISPLAY_3	:	expectedSegment = 8'b01001111;
+			DISPLAY_4	:	expectedSegment = 8'b01100110;
+			DISPLAY_5	:	expectedSegment = 8'b01101101;
+			DISPLAY_6	:	expectedSegment = 8'b01111101;
+			DISPLAY_7	:	expectedSegment = 8'b00000111;
+			DISPLAY_8	:	expectedSegment = 8'b11111111;
+			DISPLAY_9	:	expectedSegment = 8'b01100111;
+		endcase
+		expectedDisplay = {expectedDisplay[DISPLAY_MSB-9:0], expectedSegment};// bottom end of BCD
+	end
+end
+endtask
 
 //
 // Synchronous clock logic
