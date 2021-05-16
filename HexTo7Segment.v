835b3949dd4c1f239ce9c959046ee9c2ed4cd4d1
Henry-Fielding <78751620+Henry-Fielding@users.noreply.github.com>
2021-05-16 11:12:49 +0100

    HexTo7Segment - Modified input output relationships to display BCD

--- HexTo7Segment.v
+++ HexTo7Segment.v
@@ -3,6 +3,7 @@
 //-----------------------------
 //By: Henry Fielding
 //Date: 14/03/2021
+//MOdified: 16/05/2021
 //
 //Description
 //------------
@@ -18,14 +19,13 @@ module HexTo7Segment (
 localparam DISPLAY_0 = 4'h0;
 localparam DISPLAY_1 = 4'h1;
 localparam DISPLAY_2 = 4'h2;
-localparam DISPLAY_3 = 4'h4;
-localparam DISPLAY_4 = 4'h8;
-
-localparam DISPLAY_E = 4'hC;
-localparam DISPLAY_r = 4'hD;
-localparam DISPLAY_o = 4'hE;
-
-localparam DISPLAY_BLANK = 4'hF;
+localparam DISPLAY_3 = 4'h3;
+localparam DISPLAY_4 = 4'h4;
+localparam DISPLAY_5 = 4'h5;
+localparam DISPLAY_6 = 4'h6;
+localparam DISPLAY_7 = 4'h7;
+localparam DISPLAY_8 = 4'h8;
+localparam DISPLAY_9 = 4'h9;
 
 // Case statement to convert input Hexvalve to seven segment display valve
 always @ * begin
@@ -35,11 +35,11 @@ always @ * begin
 		DISPLAY_2	:	segments = 8'b01011011;
 		DISPLAY_3	:	segments = 8'b01001111;
 		DISPLAY_4	:	segments = 8'b01100110;
-
-		DISPLAY_E	:	segments = 8'b01111001;
-		DISPLAY_r	:	segments = 8'b01010000;
-		DISPLAY_o	:	segments = 8'b01011100;
-		DISPLAY_BLANK	:	segments = 8'b00000000;
+		DISPLAY_5	:	segments = 8'b01101101;
+		DISPLAY_6	:	segments = 8'b01111101;
+		DISPLAY_7	:	segments = 8'b00000111;
+		DISPLAY_8	:	segments = 8'b11111111;
+		DISPLAY_9	:	segments = 8'b01100111;
 
 		default:	segments = 8'b01000000; // display - character
 	endcase
