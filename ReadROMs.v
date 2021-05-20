23e500da59c9eca40d364e39877e0acc2509374f
Henry-Fielding <78751620+Henry-Fielding@users.noreply.github.com>
2021-05-20 18:36:25 +0100

    ReadROMS - final formatting/ commenting
    - final update to module formatting/ commenting

--- ReadROMs.v
+++ ReadROMs.v
@@ -1,21 +1,22 @@
-/*
- * Read ROMs
- * ------------------------
- * By: Henry Fielding
- * For: University of Leeds
- * Date: 13th May 2021
- *
- * Short Description
- * -----------------
- * This module is designed to read the data stored at the given address in the selected ROM module
- *
- */
+//
+// Read ROMs
+// ------------------------
+// By: Henry Fielding
+// For: University of Leeds
+// Date: 13th May 2021
+//
+// Short Description
+// -----------------
+// This module outputs RGB565 pixel data stored at the input address in the 
+// selected ROM module, functioning as a 16 bit mulitplexer. 
+// Unused ROMids output 0x0000 for all input addresses.
+
 
 module ReadROMs (
 	// declare ports
-	input 			clock,	// timing ports
-	input 			reset,
-	input [ 3:0]	ROMId,	// general ports
+	input				clock,
+	input				reset,
+	input	[ 3:0]	ROMId,
 	input	[15:0]	ROMAddr,
 	
 	output reg	[15:0]	ReadROMOut
@@ -31,95 +32,99 @@ wire	[15:0]	ROMOutBus	[15:0];
 //
 MarioWalk1	ROM0 (
 	// define port connections
-	.clock 	(clock			),
+	.clock	(clock			),
 	.address	(ROMAddr			),
 	.q			(ROMOutBus[0]	)
 );
 
 MarioWalk2	ROM1 (
 	// define port connections
-	.clock 	(clock			),
+	.clock	(clock			),
 	.address	(ROMAddr			),
 	.q			(ROMOutBus[1]	)
 );
 
 MarioWalk3	ROM2 (
 	// define port connections
-	.clock 	(clock			),
+	.clock	(clock			),
 	.address	(ROMAddr			),
 	.q			(ROMOutBus[2]	)
 );
 
 MarioJump	ROM3 (
 	// define port connections
-	.clock 	(clock			),
+	.clock	(clock			),
 	.address	(ROMAddr			),
 	.q			(ROMOutBus[3]	)
 );
 
 MarioCrouch	ROM4 (
 	// define port connections
-	.clock 	(clock			),
+	.clock	(clock			),
 	.address	(ROMAddr			),
 	.q			(ROMOutBus[4]	)
 );
 
 FloorTile	ROM5 (
 	// define port connections
-	.clock 	(clock			),
+	.clock	(clock			),
 	.address	(ROMAddr			),
 	.q			(ROMOutBus[5]	)
 );
 
 Goomba1 ROM6 (
 	// define port connections
-	.clock 	(clock			),
+	.clock	(clock			),
 	.address	(ROMAddr			),
 	.q			(ROMOutBus[6]	)
 );
 
 Goomba2 ROM7 (
 	// define port connections
-	.clock 	(clock			),
+	.clock	(clock			),
 	.address	(ROMAddr			),
 	.q			(ROMOutBus[7]	)
 );
 
 BulletBill ROM8 (
 	// define port connections
-	.clock 	(clock			),
+	.clock	(clock			),
 	.address	(ROMAddr			),
 	.q			(ROMOutBus[8]	)
 );
 
 IntroScreen ROM9 (
 	// define port connections
-	.clock 	(clock			),
+	.clock	(clock			),
 	.address	(ROMAddr			),
 	.q			(ROMOutBus[9]	)
 );
 
 GameOverScreen ROM10 (
 	// define port connections
-	.clock 	(clock			),
+	.clock	(clock			),
 	.address	(ROMAddr			),
 	.q			(ROMOutBus[10]	)
 );
 
 Background	ROM11 (
 	// define port connections
-	.clock 	(clock			),
+	.clock	(clock			),
 	.address	(ROMAddr			),
 	.q			(ROMOutBus[11]	)
 );
 
+// assign known value to unused multiplexer inputs
+assign ROMOutBus[12] = 16'd0;
+assign ROMOutBus[13] = 16'd0;
+assign ROMOutBus[14] = 16'd0;
+assign ROMOutBus[15] = 16'd0;
+
 //
 // Declare module logic
 //
-
 always @(posedge clock) begin
-	ReadROMOut <= ROMOutBus[ROMId];
+	ReadROMOut	<= ROMOutBus[ROMId];	// connects the module output to the desired ROM module
 end
 
-endmodule
-		
+endmodule 
